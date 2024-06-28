451a0cce01c271969d847e17732403374823cf09
Pam Halligan-Sims <pamhsims@yahoo.com>
2024-06-28 11:50:01 +0100

    added mock data and interface

---
+++ src/testData/recipes.ts
@@ -0,0 +1,25 @@
+export const mockRecipes = {
+  recipes: [
+    {
+      id: 652825,
+      title: 'My Asian Calamari',
+      image: 'https://img.spoonacular.com/recipes/652825-556x370.jpg',
+      imageType: 'jpg',
+      summary: `My Asian Calamari requires around <b>45 minutes</b> from start to finish. Watching your figure? This dairy free and pescatarian recipe has <b>563 calories</b>, <b>24g of protein</b>, and <b>5g of fat</b> per serving. For <b>$2.6 per serving</b>, this recipe <b>covers 23%</b> of your daily requirements of vitamins and minerals. This recipe serves 4. This recipe is typical of Asian cuisine. A mixture of all spice powder, cornstarch, squid rings, and a handful of other ingredients are all it takes to make this recipe so delicious. 3 people have made this recipe and would make it again. It works well as a rather inexpensive hor d'oeuvre. It is brought to you by Foodista. Taking all factors into account, this recipe <b>earns a spoonacular score of 59%</b>, which is solid. Users who liked this recipe also liked <a href="https://spoonacular.com/recipes/asian-calamari-salad-586727">Asian Calamari Salad</a>, <a href="https://spoonacular.com/recipes/southeast-asian-rice-noodles-with-calamari-and-herbs-188585">Southeast Asian Rice Noodles with Calamari and Herbs</a>, and <a href="https://spoonacular.com/recipes/christmas-eve-calamari-calamari-in-umido-per-la-vigilia-di-natale-597284">Christmas Eve Calamari | Calamari in Umido per la Vigilia di Natale</a>.`,
+    },
+    {
+      id: 632778,
+      title: 'Artisan Farfalle Pasta With Smoked Salmon and Cream Sauce',
+      image: 'https://img.spoonacular.com/recipes/632778-556x370.jpg',
+      summary:
+        'Artisan Farfalle Pasta With Smoked Salmon and Cream Sauce might be just the main course you are searching for. This recipe makes 2 servings with <b>762 calories</b>, <b>28g of protein</b>, and <b>48g of fat</b> each. For <b>$6.5 per serving</b>, this recipe <b>covers 31%</b> of your daily requirements of vitamins and minerals. It is brought to you by Foodista. Only a few people made this recipe, and 3 would say it hit the spot. From preparation to the plate, this recipe takes approximately <b>45 minutes</b>. If you have chicken broth, artisan pasta, extra virgin oil, and a few other ingredients on hand, you can make it. Overall, this recipe earns a <b>pretty good spoonacular score of 78%</b>. Similar recipes include <a href="https://spoonacular.com/recipes/farfalle-pasta-with-smoked-salmon-and-cream-cheese-1233493">Farfalle Pasta With Smoked Salmon And Cream Cheese</a>, <a href="https://spoonacular.com/recipes/farfalle-pasta-with-smoked-salmon-and-cream-cheese-30071">Farfalle Pasta With Smoked Salmon And Cream Cheese</a>, and <a href="https://spoonacular.com/recipes/smoked-salmon-and-farfalle-in-lemon-cream-sauce-321798">Smoked Salmon and Farfalle in Lemon Cream Sauce</a>.',
+    },
+    {
+      id: 657537,
+      title: 'Quick and Easy Caprese Salad',
+      image: 'https://img.spoonacular.com/recipes/657537-556x370.jpg',
+      summary:
+        'Need a <b>gluten free, lacto ovo vegetarian, and primal main course</b>? Quick and Easy Caprese Salad could be an awesome recipe to try. One serving contains <b>350 calories</b>, <b>15g of protein</b>, and <b>27g of fat</b>. For <b>$1.97 per serving</b>, this recipe <b>covers 20%</b> of your daily requirements of vitamins and minerals. This recipe serves 4. It is brought to you by Foodista. A mixture of balsamic vinegar, basil leaves, bell pepper, and a handful of other ingredients are all it takes to make this recipe so delicious. 3 people found this recipe to be yummy and satisfying. It is an <b>affordable</b> recipe for fans of Mediterranean food. From preparation to the plate, this recipe takes approximately <b>45 minutes</b>. Overall, this recipe earns a <b>good spoonacular score of 74%</b>. Try <a href="https://spoonacular.com/recipes/quick-caprese-salad-551296">Quick Caprese Salad</a>, <a href="https://spoonacular.com/recipes/quick-roasted-tomato-caprese-pasta-salad-673329">Quick Roasted Tomato Caprese Pasta Salad</a>, and <a href="https://spoonacular.com/recipes/easy-caprese-salad-524331">Easy Caprese Salad</a> for similar recipes.',
+    },
+  ],
+};
