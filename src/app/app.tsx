237e1529c2a36fd6b4dcd50971d769bbe0227204
Pam Halligan-Sims <pamhsims@yahoo.com>
2024-06-28 10:31:35 +0100

    init

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Home from 'src/pages/Home/Home';
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="Junior-Booster" />
+     <Home />
     </div>
   );
 }
