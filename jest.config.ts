2ca0a748d125da964511c01e01c065365e9cd0aa
Pam Halligan-Sims <pamhsims@yahoo.com>
2024-07-03 10:57:30 +0100

    move to nextjs

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'junior-booster',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/junior-booster',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
--- specs/index.spec.tsx
+++ specs/index.spec.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 
-import RecipeCard from './RecipeCard';
+import Page from '../src/app/page';
 
-describe('RecipeCard', () => {
+describe('Page', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<RecipeCard />);
+    const { baseElement } = render(<Page />);
     expect(baseElement).toBeTruthy();
   });
 });
