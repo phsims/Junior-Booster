09e3c0c5ad5808385035726cd3485b7c3ce0e59e
Daniel <danieljohnson20011@gmail.com>
2024-07-08 16:39:09 +0100

    feat(JB-1): initial header
    - created the title
    - inserted the icon into the heading

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,12 +1,21 @@
 import { Typography } from '@mui/material';
 import styles from './Header.module.scss';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
+import Box from '@mui/material/Box';
+
 
 export function Header() {
   return (
     <div className={styles['container']}>
-      <Typography variant="h1">Welcome to Header!</Typography>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <LocalDiningIcon sx={{ color: '#DF6751', width: '50.99px', height: '47.33px' }} />
+        <Typography variant="h1" component="h1" style={{ marginRight: '8px' }}>
+        Plan My Plate
+        </Typography>
+      </Box>
     </div>
   );
-}
+};
+
 
 export default Header;
