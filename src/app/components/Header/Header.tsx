318d650d06f07f044840366ae10272246b13da30
Daniel <danieljohnson20011@gmail.com>
2024-07-09 11:49:37 +0100

    feat(JB-1): header style update
    - updated styling to use the theme.ts
    - created a test to check if the heading has loaded on the page

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,14 +1,21 @@
 import { Typography } from '@mui/material';
-import styles from './Header.module.scss';
 import LocalDiningIcon from '@mui/icons-material/LocalDining';
 import Box from '@mui/material/Box';
 
+interface HeaderProps {
+  title: string;
+}
 
-export function Header() {
+
+export function Header({ title }: HeaderProps) {
   return (
-    <div className={styles['container']}>
+    <div>
       <Box display="flex" justifyContent="center" alignItems="center">
-        <LocalDiningIcon sx={{ color: '#DF6751', width: '50.99px', height: '47.33px' }} />
+        <LocalDiningIcon 
+          color="primary"
+          fontSize="large"
+          data-testid="local-dining-icon"
+        />
         <Typography variant="h1" component="h1" style={{ marginRight: '8px' }}>
         Plan My Plate
         </Typography>
