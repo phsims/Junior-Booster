4a1d5671b75c9b776a3c068cd7b097f9ce2e6aae
Daniel Johnson <daniel.johnson@cdl.co.uk>
2024-07-10 14:39:55 +0100

    feat(JB-7): further main page changes
    - added hero to page
    - put containers on cards

--- src/app/components/Hero2/Hero2.tsx
+++ src/app/components/Hero2/Hero2.tsx
@@ -1,79 +1,69 @@
-import styles from './Hero2.module.scss';
-import React from 'react'
-import { Grid, Paper, Typography } from '@mui/material';
+"use client"
+import React from 'react';
+import { Grid, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import Image from 'next/image'
-
+import Theme from '../../../theme';
+ 
 export interface HeroProps {
-  logo?: string,
-  description: string,
-  title: string
+  logo?: string;
+  description: string;
+  title: string;
 }
-
-export function Hero2({ logo, description, title}: HeroProps) {
+ 
+console.log('work')
+ 
+export function Hero2({ logo, description, title }: HeroProps) {
   return (
-    <div className={styles['container']}>
-      <Box  sx={(theme) => ({
-          width: '100%',
-          height: '503px',
-          padding: '40px 136.5px 64px 112.5px',
-          backgroundColor: theme.palette.grey[50]
-        })}>
-        <Grid container spacing={2} sx={{
-            width: '1176px',
-            height: '399px',
-            position: 'relative',
-            top: '40px',
-            left: '112.5px',
-            gap: '0px',
-       
+    <Box  sx={{backgroundColor: Theme.palette.grey[50]}}>
+       <Container>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={12}
+          md={8}
+        >
+          <Box
+            my="24px"
+            gap="24px"
+            p="24px"
+            md={{textAlign: 'left' }}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              textAlign: { xs: 'center', md: 'left' }
+            }}
+          >
+            <Typography variant="h1">{title}</Typography>
+            <Typography variant="body1">{description}</Typography>
+          </Box>
+        </Grid>
+ 
+        <Grid item xs={12} md={4}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
           }}>
-            <Grid item xs={8}>
-              <Container
-                sx={{
-                  width: '784px',
-                  height: '399px',
-                  padding: '24px 0px 71.5px 24px',
-                  gap: '24px'
-                }}
-              >
-                <Box>
-                  <Typography variant="h1" sx={{
-                    width: '760px',
-                    height: '88px',
-                    padding: '0px 68.36px 123.5px 0px',
-                    gap: '0px',
-                  }}>
-                    {title}
-                  </Typography>
-                </Box>
-                <Box>
-                  <Typography variant="body1" sx={{
-                    width: '760px',
-                    height: '72px',
-                    padding: '0px 9.11px 255.5px 0px',
-                    gap: '0px'
-                  }}
-                    >{description}
-                    </Typography>
-                </Box>
-              </Container>
-            </Grid>
-            <Grid item xs={4}>
-              <Container>
-              {logo&& <Image
-                 src={logo}
-                 width="368"
-                 alt='logo'
-                 height="368"
-                  />}
-              </Container>
-            </Grid>
-          </Grid>
-      </Box>
-    </div>
+          <Box
+            component="img"
+            sx={{
+              height: { xs: 100, md: 250 },
+              width: { xs: 100, md: 250 },
+              paddingBottom: 2
+            }}
+            alt="The house from the offer."
+            src={logo}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  </Box>
   );
 }
-
+ 
+ 
+ 
+ 
+ 
+ 
 export default Hero2;
