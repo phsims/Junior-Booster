2a7623e66bdd91e72114565b2149ccf02f1e0f9c
Jonny Lester <jonathan.edward.lester@gmail.com>
2024-07-11 12:26:21 +0100

    adding dynamic rendering

--- src/app/api/recipe/api.ts
+++ src/app/api/recipe/api.ts
@@ -1,6 +1,6 @@
 import {recipiData} from '../../components/Cards/Cards'
 
-const APIKEY = process.env.NEXT_PRIVATE_SPOONACULAR_API_KEY;
+const APIKEY = 'cafb6cd25e8c42f8b25dbfae5193e6ea';
 const BASEURL = process.env.NEXT_PUBLIC_SPOONACULAR_BASE_URL;
 const IMGURL = process.env.NEXT_PUBLIC_SPOONACULAR_IMG_URL;
 
@@ -18,4 +18,19 @@ export async function fetchRecipes(): Promise<recipiData[]> {
     return []; 
   }
 }
- 
+
+
+export async function fetchRecipeByID(id: string): Promise<recipiData[]> {
+  try {
+    const response = await fetch(`${BASEURL}/recipes/${id}/information?apiKey=${APIKEY}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data)
+    return data.recipes; 
+  } catch (error) {
+    console.log('Error fetching data:', error);
+    return []; 
+  }
+}
